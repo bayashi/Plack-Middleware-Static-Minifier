ed4e4d62ff32699f825400ead6da08b22733745c
bayashi <bayashi@cpan.org>
2011-01-04 19:51:32 +0900

    fixed minifier logic and require module name

--- lib/Plack/Middleware/Static/Minifier.pm
+++ lib/Plack/Middleware/Static/Minifier.pm
@@ -7,8 +7,6 @@ use parent 'Plack::Middleware::Static';
 use CSS::Minifier::XS qw//;
 use JavaScript::Minifier::XS qw//;
 
-use Plack::Util::Accessor qw( path root encoding pass_through );
-
 our $VERSION = '0.01';
 
 sub call {
@@ -17,10 +15,16 @@ sub call {
 
     my $res = $self->_handle_static($env);
 
-    if ($res->[0] == 200) {
+    if ($res && $res->[0] == 200) {
         my $h = Plack::Util::headers($res->[1]);
-        if ( $h->get('Content-Type') ) {
-            _minifier($res, $h);
+        if ( $h->get('Content-Type')
+                && $h->get('Content-Type') =~ m!/(css|javascript)! ) {
+            my $m = $1;
+            my $body; Plack::Util::foreach($res->[2], sub { $body .= $_[0] });
+            $res->[2] = ($m =~ m!^css!)
+                      ? [ CSS::Minifier::XS::minify($body) ]
+                      : [ JavaScript::Minifier::XS::minify($body) ];
+            $h->set('Content-Length', length $res->[2][0]);
         }
     }
 
@@ -29,32 +33,6 @@ sub call {
     }
 
     return $self->app->($env);
-}
-
-sub _minifier {
-    my ($res, $h) = @_;
-
-    my $minified;
-    if ($h->get('Content-Type') =~ m!/css!) {
-        _get_body($res);
-        $minified = CSS::Minifier::XS::minify($res->[2][0]);
-    }
-    elsif ( $h->get('Content-Type') =~ m!/javascript! ) {
-        _get_body($res);
-        $minified = JavaScript::Minifier::XS::minify($res->[2][0]);
-    }
-    if ($minified) {
-        $res->[2] = [$minified];
-        $h->set('Content-Length', length $minified);
-    }
-}
-
-sub _get_body {
-    my $res = shift;
-
-    my $body;
-    Plack::Util::foreach($res->[2], sub { $body .= $_[0] });
-    $res->[2] = [$body];
 }
 
 1;
