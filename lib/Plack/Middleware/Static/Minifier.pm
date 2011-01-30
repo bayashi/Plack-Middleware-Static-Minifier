aba207fef43938960b92abf8caa89099d34250d8
bayashi <bayashi@cpan.org>
2011-01-30 12:03:48 +0900

    prepare for CPAN dist

--- lib/Plack/Middleware/Static/Minifier.pm
+++ lib/Plack/Middleware/Static/Minifier.pm
@@ -6,7 +6,7 @@ use parent 'Plack::Middleware::Static';
 use CSS::Minifier::XS qw//;
 use JavaScript::Minifier::XS qw//;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub call {
     my $self = shift;
